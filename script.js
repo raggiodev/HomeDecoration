5f768820bdb4928ac2b281758e04876dd70d2dad
JoeTheorium <fernandoandresraggio@gmail.com>
2023-05-22 23:35:02 -0300

    Add scroll button & upgrades

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
 
 
-document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide', {
-        type : 'loop',
-        drag : 'free',
-        snap : true,
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        drag: 'free',
+        snap: true,
         perPage: 1,
         perMove: 1,
-    } );
+    });
     splide.mount();
-} );
+});
+
+// SCROLL BUTTON TO TOP
+const scrollButton = document.getElementById("scrollButton");
+
+function scrollToTop() {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
+
+// HIDDE BUTTON
+function scrollFunction() {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    scrollButton.classList.toggle("show", scrollTop > 20);
+}
+
+// CALL FUNCTION
+window.addEventListener("scroll", scrollFunction);
